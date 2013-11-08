4230167b4bf52db0910e415f04869ce8a33817f5
Johnson Liu <jliu@xtremeprog.com>
2013-11-08 18:20:37 +0800

    go

--- apps/p2p_server/include/p2p_server.hrl
+++ apps/p2p_server/include/p2p_server.hrl
@@ -1,2 +1,2 @@
 %%%% database tables =============================
--record(run_user, {id, local_ip, local_port, public_ip, public_port, createdTime}).
+-record(run_user, {id, pid, local_ip, local_port, public_ip, public_port, createdTime}).
